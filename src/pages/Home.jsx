ad61d006e3aada8979445783585792f17303f0f3
ghkdtjsdud0312 <ghkdtjsdud0312@gmail.com>
2023-10-30 11:17:09 +0900

    10/30

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,15 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 const Home = () => {
+  // Link to를 사용하지 않고 다른 페이지로 이동해야 할 상황에서 사용하는 Hook
+  const navigate = useNavigate();
   return (
     <>
       <h1>여기가 홈 입니다.</h1>
       <p>가장 먼저 보여지는 페이지 입니다.</p>
       <Link to="/about">소개페이지로 이동</Link>
+      <br />
+      <button onClick={() => navigate("/mypage")}>마이페이지로 이동</button>
       <br />
       <Link to="/MyPage">마이페이지로 이동</Link>
       <br />
--- src/pages/MyPage.jsx
+++ src/pages/MyPage.jsx
@@ -1,11 +1,13 @@
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 const MyPage = () => {
+  const navigate = useNavigate();
   return (
     <>
       <h1>MyPage</h1>
       <p>여기는 MyPage 입니다.</p>
-      <Link to="/">Home으로 이동</Link>
+      {/* <Link to="/">Home으로 이동</Link> */}
+      <button onClick={() => navigate(-1)}>뒤로 가기</button>
     </>
   );
 };
